20c603fd44cf85192dfa539bad522c3d3ab8e064
Aidar <aydar.nizamov.98@mail.ru>
2022-04-04 03:27:10 +0300

    Delete chapter_7/chapter_7 directory

--- chapter_7/chapter_7/header.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  header.cpp
-//  chapter_7
-//
-//  Created by Айдар Низамов on 21.03.2020.
-//  Copyright © 2020 Айдар Низамов. All rights reserved.
-//
-
-#include "header.hpp"
--- chapter_7/chapter_7/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "header.hpp"
-
-
-int main() {
-    string name = "Module Zero";
-    print(name);
-}
